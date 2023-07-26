b1acdc4936a08833ca18081dba4761f25c24642d
owen-eternal <ptarworx@gmail.com>
2023-07-26 21:31:41 +0200

    Define variables for the launch template

--- variable.tf
+++ variable.tf
@@ -1,5 +1,12 @@
-variable vpc_cdir {}
-variable project_name {}
-variable web_server_port {}
-variable db_server_port {}
-variable subnet_cdir {}
+####### VPC Module Variables ########
+variable "vpc_cdir" {}
+variable "project_name" {}
+variable "web_server_port" {}
+variable "db_server_port" {}
+variable "subnet_cdir" {}
+
+####### APP-Tier Module Variables ########
+variable "app_ami" {}
+variable "app_instance_type" {}
+variable "user_data" {}
+variable "ssh_key_pair" {}
