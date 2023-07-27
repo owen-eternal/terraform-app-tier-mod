cf84aa671c11796ea34c60958a64eded9a7348b2
owen-eternal <ptarworx@gmail.com>
2023-07-27 18:50:39 +0200

    Replace nginx with apache server

--- nginx.sh
+++ nginx.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo yum update -y
-sudo amazon-linux-extras install nginx1 -y 
-sudo systemctl enable nginx
-sudo systemctl start nginx
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
