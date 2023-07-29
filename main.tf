767e52cc7da5c930c012850ef0c093bdf05f1f9b
owen-eternal <ptarworx@gmail.com>
2023-07-29 15:17:27 +0200

    Add a code snippet to create Target a group

--- main.tf
+++ main.tf
@@ -65,3 +65,15 @@ resource "aws_lb_listener" "terramino" {
     target_group_arn = aws_lb_target_group.web-tg.arn
   }
 }
+
+resource "aws_lb_target_group" "web-tg" {
+  name     = "${module.tf-aws-network.tag_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.tf-aws-network.vpc_id
+}
+
+resource "aws_autoscaling_attachment" "app-atg-tg-att" {
+  autoscaling_group_name = aws_autoscaling_group.web-asg.id
+  lb_target_group_arn   = aws_lb_target_group.web-tg.arn
+}
