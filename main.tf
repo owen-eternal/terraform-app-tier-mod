5a9b21c8298fb37ae7bc5f4764cfb0935e700fa5
owen-eternal <ptarworx@gmail.com>
2023-08-16 01:25:02 +0200

    Add data source lookup for ECS optimised AMI

--- main.tf
+++ main.tf
@@ -1,9 +1,35 @@
 #################################################
-################ local #########################
+################ local ##########################
 #################################################
 
 locals {
   http_port = var.web_port
+}
+
+################################################
+############### Resources ######################
+################################################
+
+# Amazon ECS Optimised image
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs-hvm-*-x86_64-ebs"]
+  }
+
+  owners = ["amazon"]
 }
 
 ################################################
